d64a4df669ffa82a35b007fa0b4875d53edaa95c
oxleberry <oxleberry@gmail.com>
2018-07-07 11:45:18 -0700

    added basic nav and info container layout

--- static-site/js/app.js
+++ static-site/js/app.js
@@ -1,8 +1,11 @@
 
 $(function(){
     console.log('Sanity Check');
-
-
+    
+    // NAV ACTIONS
+    $('.toggle-nav').on('click',function() {
+      $('.flex-nav ul').toggleClass('nav-active');
+   });
 
 
 }); // end of document.ready
